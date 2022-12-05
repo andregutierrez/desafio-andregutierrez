38906fbf0c43ec35c8de8737eb0788c35ffe58f0
André Gutierrez <andregutierrez@gmail.com>
2022-12-04 21:02:55 -0300

    exclusão da string de conexão

--- src/AndreGutierrez.Infra.Data/DesafioContextFactory.cs
+++ src/AndreGutierrez.Infra.Data/DesafioContextFactory.cs
@@ -8,7 +8,7 @@ public class DesafioContextFactory : IDesignTimeDbContextFactory<DesafioContext>
     public DesafioContext CreateDbContext(string[] args)
     {
         var optionsBuilder = new DbContextOptionsBuilder<DesafioContext>();
-        optionsBuilder.UseSqlServer("Server=tcp:flt-sqlserver.database.windows.net,1433;Initial Catalog=flt-database;Persist Security Info=False;User ID=fltadmin;Password=!R@m0n3s#$#;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;");
+        optionsBuilder.UseSqlServer("");
         return new DesafioContext(optionsBuilder.Options);
     }
 }
